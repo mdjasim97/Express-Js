0643217e6b3ad69d61f94a7d50fb83fe15f582ec
MD JASIM UDDIN <mjunachole97@gmail.com>
2024-10-16 22:41:26 +0600

    Express Js routeting

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ app.get('/', (req, res) => {
     res.send('Hello Express Js.')
 })
 
+app.post('/about', (req, res) => {
+    res.send('About page')
+})
+
+app.put('/contact', (req, res) => {
+    res.send('Contact page')
+})
+
+app.delete('/services', (req, res) => {
+    res.send('Services page')
+})
+
 
 app.listen(5000, () => {
     console.log('Server running port is 5000')
